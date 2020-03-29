77bdfb68465ee401b17aab961ec4cd80a802f18f
chrisjamesr <chrisjames.r@gmail.com>
2020-03-29 16:44:50 -0400

    add simple node server and function to occupy even loop for 5 seconds

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// duration in ms
+function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {};
+};
+
+app.get('/', (req, res)=>{
+    doWork(5000);
+    res.send('hello');
+})
+
+app.listen(3000, () => console.log('Listening on port: 3000'))
