2d9088153f72327e2b29e9bb14f9d41686df605c
chrisjamesr <chrisjames.r@gmail.com>
2020-03-29 17:50:24 -0400

    add threadpool function to route function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+process.env.UV_THREADPOOL_SIZE = 1   
 const cluster = require('cluster');
 // is the file being executed in master mode?
 if (cluster.isMaster) {
@@ -5,17 +6,17 @@ if (cluster.isMaster) {
 } else {
     const express = require('express');
     const app = express();
-
-    // duration in ms
-    function doWork(duration) {
-        const start = Date.now();
-        while(Date.now() - start < duration) {};
-    };
-
-    app.get('/', (req, res)=>{
-        doWork(5000);
-        res.send('hello');
+    const crypto = require('crypto')
+    
+    app.get('/', (req, res) => {
+        crypto.pbkdf2('a', 'b', 10000, 512, 'sha512', () => {
+            res.send('Hi there');
+        });
     });
+
+    app.get('/fast', (req, res) => {
+        res.send('This was fast');
+    })
 
     app.listen(3000, () => console.log('Listening on port: 3000'))
 }
