846c4dc8b674f3311808515e8319ba1a39523d77
chrisjamesr <chrisjames.r@gmail.com>
2020-03-29 16:58:47 -0400

    add conditional to create master server instance or fork

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
+// is the file being executed in master mode?
+if (cluster.isMaster) {
+    cluster.fork();
+} else {
+    const express = require('express');
+    const app = express();
 
-// duration in ms
-function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {};
-};
+    // duration in ms
+    function doWork(duration) {
+        const start = Date.now();
+        while(Date.now() - start < duration) {};
+    };
 
-app.get('/', (req, res)=>{
-    doWork(5000);
-    res.send('hello');
-})
+    app.get('/', (req, res)=>{
+        doWork(5000);
+        res.send('hello');
+    });
 
-app.listen(3000, () => console.log('Listening on port: 3000'))
+    app.listen(3000, () => console.log('Listening on port: 3000'))
+}
